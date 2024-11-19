1a9e13b94514e92ae1f9835eab688acfdf0107e6
ShowGa <showgacareer@gmail.com>
2024-11-19 12:25:06 +0800

    <Progress, Installation> 1. Progress : User Login Page UI 2. Installation : Material UI library

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layouts from "./layouts/Layouts";
 // pages
 import Home from "./pages/Home";
+import UserLogin from "./pages/UserLogin";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<Layouts />}>
           <Route index element={<Home />} />
         </Route>
+        <Route path="/user-login" element={<UserLogin />} />
       </Routes>
     </BrowserRouter>
   );
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import img1 from "./images/1.jpg";
 import img2 from "./images/2.jpg";
+import img3 from "./images/3.jpg";
 
-export { img1, img2 };
+export { img1, img2, img3 };
