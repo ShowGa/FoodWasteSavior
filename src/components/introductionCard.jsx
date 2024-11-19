2ad5194353f3ae932f71216338af21bd8aeea6e5
ShowGa <showgacareer@gmail.com>
2024-11-19 10:49:39 +0800

    <Modify> 1. Modify the IntroductionCard class naming

--- src/components/introductionCard.jsx
+++ src/components/introductionCard.jsx
@@ -3,11 +3,11 @@ import "./css/componentsCSS.css";
 
 const IntroductionCard = ({ icon, title, content }) => {
   return (
-    <div className="card">
-      <div className="card-icon">{icon}</div>
-      <div className="card-content">
-        <h1 className="card-title">{title}</h1>
-        <p className="card-text">{content}</p>
+    <div className="c-introduction-card">
+      <div className="c-introduction-card-icon">{icon}</div>
+      <div className="c-introduction-card-content">
+        <h1 className="c-introduction-card-title">{title}</h1>
+        <p className="c-introduction-card-text">{content}</p>
       </div>
     </div>
   );
