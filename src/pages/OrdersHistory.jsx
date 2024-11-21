6705d018d1880ee80847c4836dc52ad9665e4e73
ShowGa <showgacareer@gmail.com>
2024-11-21 14:55:42 +0800

    <Progress> 1. Progress : Order and OrderHistory page structure

--- src/pages/OrdersHistory.jsx
+++ src/pages/OrdersHistory.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const OrdersHistory = () => {
-  return <div>OrdersHistory</div>;
+  return (
+    <main className="p-[1.5rem]">
+      <div className="flex flex-col p-[1.5rem] min-h-screen bg-red-200">
+        {/* title */}
+        <div className="text-center">
+          <h1 className="text-4xl">歷史訂單</h1>
+        </div>
+
+        <div>
+          <h2>2024-12-12</h2>
+
+          {/* card */}
+          <div>card</div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default OrdersHistory;
