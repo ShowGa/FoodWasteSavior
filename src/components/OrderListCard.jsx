e24beefbee027648eae3206c2c5e91e492ec6a2a
ShowGa <showgacareer@gmail.com>
2024-11-27 14:29:39 +0800

    <Progress> 1. Progress : Orders.jsx and OrderListCard.jsx

--- src/components/OrderListCard.jsx
+++ src/components/OrderListCard.jsx
@@ -1,18 +1,28 @@
 import React from "react";
+// img
+import { img1 } from "../assets";
 
-const OrderListCard = () => {
+const OrderListCard = ({ prop }) => {
   return (
-    <div>
-      <div>Pending Collection</div>
+    <div className="c-order-list-card">
+      <div className="rounded-full border border-black absolute top-0 left-0 bg-white px-2 translate-y-[-50%]">
+        <span>Pending Collection</span>
+      </div>
 
-      <div>
-        <div>
-          <img src="" alt="" />
+      <div className="flex justify-start items-center gap-4">
+        <div className="rounded-full border-2 border-gray-300 w-[3rem] h-[3rem]">
+          <img
+            src={img1}
+            alt=""
+            className="rounded-full w-full h-full object-cover"
+          />
         </div>
 
         <div>
-          <h3>Order Name</h3>
-          <p>Collection Date & Time</p>
+          <h3 className="text-xl">Super Bag</h3>
+          <p className="text-xs font-light">
+            11月27日 10:00 AM - 11月27日 11:00 AM
+          </p>
         </div>
       </div>
     </div>
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+// components
+import OrderListCard from "../components/OrderListCard";
 
 const Orders = () => {
   return (
     <main className="p-[1.5rem]">
-      <div className="flex flex-col p-[1.5rem] min-h-screen bg-red-200">
+      <div className="flex flex-col p-[1.5rem] min-h-screen">
         {/* title */}
         <div className="text-center">
           <h1 className="text-4xl">訂單</h1>
@@ -13,7 +15,11 @@ const Orders = () => {
           <h2>2024-12-12</h2>
 
           {/* card */}
-          <div>card</div>
+          <div className="flex flex-wrap gap-4 mt-8">
+            <OrderListCard />
+            <OrderListCard />
+            <OrderListCard />
+          </div>
         </div>
       </div>
     </main>
